71bad977bfe64d01d714cdf140f9912b2a63868f
JamesWattersIV <53174355+JamesWattersIV@users.noreply.github.com>
2020-10-03 09:42:42 +0200

    update fies to use types-script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Delish Foods`,
+    description: `Delish Foods providing you with the best products at imported prices`,
+    author: `James Watters <wattersjamesc@gmail.com>`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Delish Foods`,
+        short_name: `Delish`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
